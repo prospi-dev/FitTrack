ffedd57a53c7dfb9f87d3ad0af64242c3b515af5
prospi-dev <manuprosperi04@gmail.com>
2026-02-23 20:14:09 +0000

    JwtService added

--- Backend/FitTrack/obj/Debug/net10.0/FitTrack.AssemblyInfo.cs
+++ Backend/FitTrack/obj/Debug/net10.0/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc92bc7a672e1838ffcd9624295eee4636a127b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58728d46357678893db7318312fde008d05600ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
