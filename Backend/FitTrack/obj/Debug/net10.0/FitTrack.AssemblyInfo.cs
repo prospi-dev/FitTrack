088bd48708a22fd6edb7e6ffcbb27061fb8b951b
prospi-dev <manuprosperi04@gmail.com>
2026-02-23 20:27:55 +0000

    DTOs and auth controller done

--- Backend/FitTrack/obj/Debug/net10.0/FitTrack.AssemblyInfo.cs
+++ Backend/FitTrack/obj/Debug/net10.0/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58728d46357678893db7318312fde008d05600ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2518104f324d3f3a2efa77113bca07d17accb697")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
