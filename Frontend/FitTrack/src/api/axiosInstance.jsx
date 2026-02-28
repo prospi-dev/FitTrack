568ff049af9fc2b27ca09dba78c07346fb62a593
prospi-dev <manuprosperi04@gmail.com>
2026-02-28 23:29:37 +0000

    Change port

--- Frontend/FitTrack/src/api/axiosInstance.jsx
+++ Frontend/FitTrack/src/api/axiosInstance.jsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 // })
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://localhost:7239/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://localhost:5173/api',
 })
 
 console.log(import.meta.env.VITE_API_URL)
