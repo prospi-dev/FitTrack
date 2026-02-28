3e713742950d36ab2da82ece63cae0dc53eb4f16
prospi-dev <manuprosperi04@gmail.com>
2026-02-28 22:07:56 +0000

    Title logo changed

--- Backend/FitTrack/obj/Debug/net10.0/FitTrack.AssemblyInfo.cs
+++ Backend/FitTrack/obj/Debug/net10.0/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a193555890d51926b46636fc1d49a274dcfdd9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8db83f9ac9b8a0114d7024145bde8abbf795db")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
