6b9e16220e0a6d44652220a413c5a6d40b8bd344
prospi-dev <manuprosperi04@gmail.com>
2026-03-06 17:08:17 +0000

    emailjs fixed

--- Frontend/FitTrack/src/pages/public/AboutPage.jsx
+++ Frontend/FitTrack/src/pages/public/AboutPage.jsx
@@ -15,14 +15,14 @@ export default function AboutPage() {
 
     try {
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        import.meta.env.local.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.local.VITE_EMAILJS_TEMPLATE_ID,
         {
           from_name: name,
           from_email: email,
           message,
         },
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        import.meta.env.local.VITE_EMAILJS_PUBLIC_KEY
       )
       setStatus('success')
       setName('')
